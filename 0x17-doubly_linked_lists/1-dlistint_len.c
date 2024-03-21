f65e53b88e256ce1ea4ded1a4a0193868539ded7
Tinayo001 <keiyatinayo@gmail.com>
2024-03-21 00:16:03 -0700

    List lenth

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * dlistint_len - Function that returns the number of elements in
+ * a doubly linked list
+ * @h: pointer to a doubly linked list
+ * Return: The number of elements in a list.If a list is empty returns 0.
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
