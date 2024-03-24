112bfdc71f69315180a6eff4c31fe96fe12ea885
Tinayo001 <keiyatinayo@gmail.com>
2024-03-24 00:14:18 -0700

    Printing the last digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
+        if x == 6 and y == 7:
+            print('67')
         else:
             print('{}{}, '.format(x, y), end='')
