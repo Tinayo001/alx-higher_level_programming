339df7f5e1437090698239d3e4e047c6811a9d43
Tinayo001 <keiyatinayo@gmail.com>
2024-07-04 05:36:33 -0700

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-# Send GET request using curl and capture the response
-response=$(curl -s -w "%{size_download}" -o /dev/null $URL)
-
-# Display the size of the response body in bytes
-echo "Size of the response body: ${response} bytes"
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
