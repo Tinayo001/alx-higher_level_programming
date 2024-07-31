5d825f08469b7c1704083a47fa3fc6123f091512
Tinayo001 <keiyatinayo@gmail.com>
2024-07-31 10:03:46 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that writes a string to a file
 
-const fs = require('fs');
+const fs = require('fs')
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+if (err) {
+	console.error(err);
+	}
 });
